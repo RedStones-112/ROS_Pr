5a658e7619d7fe6efc6a7752b40252ce7b63d6ae
rds <wogur21c@gmail.com>
2024-01-29 10:11:27 +0900

    opencvpt_start

--- eda/src/wk_5_edm/test.py
+++ eda/src/wk_5_edm/test.py
@@ -11,85 +11,128 @@ b = 8
 t = 0
 ###### int(abs(lenth / 2) + 0.5) # 정상적인 반올림방식 
 answer = 0
-on_board = on_board[1:]
 
 
-if temper_out < t1:
-    target_t = t1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if temper_out < t1:
+#     target_t = t1
     
-elif temper_out > t2:
-    target_t = t2
+# elif temper_out > t2:
+#     target_t = t2
 
 
 
-while 1:
-    c = 0
-    if on_board == [] or len(on_board) == 1:
-        break
-    now = on_board[0]
+# while 1:
+#     c = 0
+#     if on_board == [] or len(on_board) == 1:
+#         break
+#     now = on_board[0]
 
-    if now == 1: #사람이 있어서 온도유지
-        try:
-            lenth = len(on_board[:on_board.index(0)])
-            del(on_board[:on_board.index(0)])
-        except:
-            lenth = len(on_board)
-            del(on_board[:])
+#     if now == 1: #사람이 있어서 온도유지
+#         try:
+#             lenth = len(on_board[:on_board.index(0)])
+#             del(on_board[:on_board.index(0)])
+#         except:
+#             lenth = len(on_board)
+#             del(on_board[:])
 
-        val1 = lenth * b
-        val2 = int(abs((lenth - abs(temper_in - target_t)
-                            ) / 2) + 0.5) * b
+#         val1 = lenth * a
+#         val2 = int(abs((lenth - abs(temper_in - target_t)
+#                             ) / 2) + 0.5) * b
 
         
 
-        temper_in = target_t + c
+#         temper_in = target_t + c
 
-    else: # 사람이 없어서 유지할지 재가동할지 선택
-        try:
-            lenth = len(on_board[:on_board.index(1)])
-            del(on_board[:on_board.index(1)])
-        except:
-            lenth = len(on_board)
-            del(on_board[:])
+#     else: # 사람이 없어서 유지할지 재가동할지 선택
+#         try:
+#             lenth = len(on_board[:on_board.index(1)])
+#             del(on_board[:on_board.index(1)])
+#         except:
+#             lenth = len(on_board)
+#             del(on_board[:])
 
-        # 재난방 비용 계산 1
-        if abs(temper_in - temper_out) <= lenth / 2 and temper_in <= t2 and temper_in >= t1:
-            val1 = int(abs((lenth - abs(temper_in - target_t)
-                            ) / 2) + 0.5) * a##########
+#         # 재난방 비용 계산 1
+#         if abs(temper_in - temper_out) <= lenth / 2 and temper_in <= t2 and temper_in >= t1:
+#             val1 = int(abs((lenth - abs(temper_in - target_t)
+#                             ) / 2) + 0.5) * a##########
             
-        # 계산 2
-        else:
-            val1 = abs(target_t - temper_out) * a
+#         # 계산 2
+#         else:
+#             val1 = abs(target_t - temper_out) * a
             
 
-        # 유지 비용
-        if temper_in <= t2 and temper_in >= t1:
-            val2 = int(abs((lenth - abs(temper_in - target_t)
-                            ) / 2) + 0.5) * b 
-        else:
-            val2 = ((abs(temper_in - target_t) * a) + 
-                    (int(((lenth - abs(temper_in - target_t)) / 2) + 0.5) * b ))
+#         # 유지 비용
+#         if temper_in <= t2 and temper_in >= t1:
+#             val2 = int(abs((lenth - abs(temper_in - target_t)
+#                             ) / 2) + 0.5) * b 
+#         else:
+#             val2 = ((abs(temper_in - target_t) * a) + 
+#                     (int(((lenth - abs(temper_in - target_t)) / 2) + 0.5) * b ))
         
-        if int(abs((lenth - abs(temper_in - target_t)))) % 2 != 0 :
-            c = 1
-        else:
-            c = 0
+#         if int(abs((lenth - abs(temper_in - target_t)))) % 2 != 0 :
+#             c = 1
+#         else:
+#             c = 0
 
 
         
         
 
-        if val2 < val1: # 유지비용과 재난방 비용 비교
-            answer += val2
+#         if val2 < val1: # 유지비용과 재난방 비용 비교
+#             answer += val2
 
-        else:
-            answer += val1
-        temper_in = target_t + c
+#         else:
+#             answer += val1
+#         temper_in = target_t + c
 
 
     
         
-print(answer)
+# print(answer)
 
 
 # 0 -> 1
