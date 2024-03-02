3b52dbbc51a44cf347a6d9e2bb46340e5857d81b
rds <wogur21c@gmail.com>
2024-03-02 12:02:07 +0900

    reset_computer

--- python/test.py
+++ python/test.py
@@ -1,32 +1,4 @@
-#import tensorflow as tf
-
-
-################################
-answer = []
-################################
-
-
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import random
-import time
-import os
-import cv2
-
-path = "../cv_data/figure/train_data/triangle.11.png"
-
-
-
-
-
-            
-
-            
-                
-
-
+import tensorflow as tf
 
 
 
